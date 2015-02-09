6eae521ac9aa5f8a65e456ca9a6545ce1afdaaad
rubenjimenezarias <rubenjimenezarias@gmail.com>
2015-02-09 09:15:28 +0100

    Modificado metodo printProductDetails

---
+++ Test.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class Test here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Test
+{
+    public void test1()
+    {
+        StockManager stockManager = new StockManager();
+        Product producto1 = new Product(1,"a");
+        Product producto2 = new Product(2,"b");
+        Product producto3 = new Product(3,"c");
+        Product producto4 = new Product(4,"d");
+        Product producto5 = new Product(5,"e");
+        Product producto6 = new Product(6,"f");
+        
+        stockManager.addProduct(producto1);
+        stockManager.addProduct(producto2);
+        stockManager.addProduct(producto3);
+        stockManager.addProduct(producto4);
+        stockManager.addProduct(producto5);
+        stockManager.addProduct(producto6);
+        
+        stockManager.printProductDetails();
+    }
+    
+    
+}
